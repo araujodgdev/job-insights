dac0efaca3d50febdad20ab011ce34e07d276dc4
Douglas Araújo <devdoug45@gmail.com>
2023-11-30 11:31:24 -0300

    feat: ProcessJobs read method

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,18 +1,23 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path: str) -> List[Dict]:
+        try:
+            with open(path, encoding='utf-8') as file:
+                file_data = csv.DictReader(file)
+                self.jobs_list = list(file_data)
+                return self.jobs_list
+        except FileNotFoundError:
+            print('Arquivo não encontrado')
+            return list()
 
     def get_unique_job_types(self) -> List[str]:
         pass
 
     def filter_by_multiple_criteria(self) -> List[dict]:
         pass
-
-
-# Iniciando o projeto
