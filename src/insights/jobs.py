1795a99055cad5603cf28c946f5746fc07f0b5ac
Douglas Araújo <devdoug45@gmail.com>
2023-11-30 12:18:47 -0300

    feat: ProcessJobs.filter_by_multiple_criteria

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -8,23 +8,34 @@ class ProcessJobs:
 
     def read(self, path: str) -> List[Dict]:
         try:
-            with open(path, encoding='utf-8') as file:
+            with open(path, encoding="utf-8") as file:
                 file_data = csv.DictReader(file)
                 self.jobs_list = list(file_data)
                 return self.jobs_list
         except FileNotFoundError:
-            print('Arquivo não encontrado')
+            print("Arquivo não encontrado")
             return list()
 
     def get_unique_job_types(self) -> List[str]:
         job_types = list()
         for job in self.jobs_list:
-            if job['job_type'] not in job_types:
-                job_types.append(job['job_type'])
+            if job["job_type"] not in job_types:
+                job_types.append(job["job_type"])
         return job_types
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(
+        self, jobs: List[Dict], filter_criteria: Dict
+    ) -> List[dict]:
+        if not isinstance(filter_criteria, dict):
+            raise TypeError("filter_criteria deve ser um dicionário")
+        filtered_jobs = list()
+        for job in jobs:
+            match_criteria = all(
+                job.get(key) == value for key, value in filter_criteria.items()
+            )
+            if match_criteria:
+                filtered_jobs.append(job)
+        return filtered_jobs
 
 
 # test = ProcessJobs()
