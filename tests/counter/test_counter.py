46a3561a47ab788020517ec8083f500b65b8588b
Douglas Araújo <devdoug45@gmail.com>
2023-11-30 11:15:23 -0300

    test: counter_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    path = 'data/jobs.csv'
+    words = ['python', 'javascript']
+    ocurrences_python = 1639
+    ocurrences_javascript = 122
+    assert count_ocurrences(path, words[0]) == ocurrences_python
+    assert count_ocurrences(path, words[1]) == ocurrences_javascript
